f3d08ea9b00d42999d8776d23c3d8c07bf7e1cac
Edmond Chuc <e.chuc@uq.edu.au>
2021-10-27 13:07:46 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cors-proxy
+
+cors-proxy is a proxy server implemented with Express.js. It is a limited clone of cors-anywhere.
---
+++ index.js
@@ -0,0 +1,52 @@
+const axios = require('axios').default
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const port = 8000
+
+app.use(cors())
+
+// TODO: Review and compare with cors-anywhere implementation.
+app.get('/:url(https?:\/\/[a-zA-Z\.\-0-9\/]*)', async (req, res) => {
+  const url = req.params.url
+  console.log(`Received URL ${url}`)
+
+  // Forward headers on.
+  const headers = req.headers
+  headers['host'] = new URL(url).host
+  const options = {
+    headers: headers,
+  }
+
+  try {
+    const urlResponse = await axios.get(url, options)
+    console.log(urlResponse.status)
+
+    // Set the headers from the URL response.
+    const responseHeaders = urlResponse.headers
+    for (const header in responseHeaders) {
+      res.set(header, responseHeaders[header])
+    }
+
+    res.send(urlResponse.data)
+    return
+  }
+  catch (error) {
+    console.error(`Error with URL ${url}`)
+    console.error(error)
+    // TODO: Review this status code.
+    res.status(422)
+    res.json(error)
+    return
+  }
+
+});
+
+app.get('/', (req, res) => {
+  res.send('cors-proxy home')
+})
+
+app.listen(port, () => {
+  console.log(`cors-proxy listening on port ${port}`)
+})
